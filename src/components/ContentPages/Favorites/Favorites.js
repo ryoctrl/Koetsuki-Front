9ecd0e670576e00fb745e79fc6c31f84767059d6
mosin <mosin@mosin.jp>
2019-04-23 22:03:18 +0900

    add feature search to favorites

--- src/components/ContentPages/Favorites/Favorites.js
+++ src/components/ContentPages/Favorites/Favorites.js
@@ -19,11 +19,19 @@ class Favorites extends Component {
         this.props.dispatch(getFav(this.props.favorites, this.props.user));
     }
     render() {
-        const { classes, circles, favorites } = this.props;
-        const displayCircles = [];
+        const { classes, circles, results, favorites } = this.props;
+        let displayCircles = [];
         for(const fav of favorites) {
-            const c = circles.filter(c => c.id === fav.circleId);
-            if(c && c[0]) displayCircles.push(c[0]);
+            let c = circles.filter(c => c.id === fav.circleId);
+            if(!c || !c[0]) continue;
+            c = c[0];
+            if(!results || results.length === circles.length) {
+                displayCircles.push(c);
+                continue;
+            }
+            c = results.filter(r => r.id === c.id);
+            if(!c[0]) continue;
+            displayCircles.push(c[0]);
         }
         return (
             <Grid container spacing={24}>
