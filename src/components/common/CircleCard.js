720534c239a13592502d0eb92c2be2eba0020045
mosin <mosin@mosin.jp>
2019-04-27 14:32:49 +0900

    change circle cut size

--- src/components/common/CircleCard.js
+++ src/components/common/CircleCard.js
@@ -15,6 +15,7 @@ const styles = theme => ({
     media: {
         height: 0,
         paddingTop: '56.25%',
+        backgroundSize: 'contain'
     },
     content: {
         padding: theme.spacing.unit
